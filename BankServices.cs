d8ba9cfd5d7e45cef53a19127222c754794eac38
AllaReshma22 <91258492+AllaReshma22@users.noreply.github.com>
2021-10-12 10:55:42 +0530

    Add files via upload

--- BankServices.cs
+++ BankServices.cs
@@ -8,94 +8,157 @@ namespace BankApp.Services
 {
     public class BankServices
     {
-        int acnumber = 1;
-        public List<Account> accountslist = new List<Account>();
-        public List<Transaction> transaclist = new List<Transaction>();
-        public void createAccount(string acname, int Pin, int initialbal)
+        Bank bank = new Bank();
+        public static int accountNumber = 1;
+        public void createAccount( string AccountName, int password, double initialbal)
         {
-            Account account = new Account
-            {
-                AccountHolderName = acname,
-                pin = Pin,
-                AccountNumber = acnumber,
-                Balance = initialbal,
-            };
-            this.accountslist.Add(account);
-            acnumber++;
+            Account account = new Account(accountNumber, AccountName, password, initialbal);
+            this.bank.AccountsList.Add(account);
+            accountNumber++;
         }
-        public bool Deposit(int amount, int accountnumber, int pin)
+        public bool Deposit(double amount, int accountnumber, int pin)
         {
-            var Accno = this.accountslist.Single(m => m.AccountNumber == accountnumber);
-            if (Accno.pin == pin)
-            {
-                Accno.Balance += amount;
-                AddTransaction(accountnumber, "deposit", DateTime.Now.ToString(), amount);
-            }
-            else
-            {
-                throw new Exception("Incorrect pin number");
-            }
-            return true;
-        }
-        public bool WithDraw(int amount, int accountnumber, int pin)
-        {
-            var Accno = this.accountslist.Single(m => m.AccountNumber == accountnumber);
-            if (Accno.pin == pin)
-            {
-                Accno.Balance -= amount;
-                AddTransaction(accountnumber, "withdraw", DateTime.Now.ToString(), amount);
-            }
-            else
-            {
-                throw new Exception("Incorrect pin number");
-            }
-            return true;
 
-        }
-      public bool transferAmount(int amount,int saccnumber,int spin,int raccnumber)
-        {
-            var Accno = this.accountslist.Single(m => m.AccountNumber == saccnumber);
-            if (Accno.pin == spin)
+            var account = this.bank.AccountsList.SingleOrDefault(m => m.AccountNumber == accountnumber);
+
+            if (account is null)
             {
-                if (Accno.Balance > amount)
+                throw new Exception("Either invalid");
+            }
+            else
+            {
+                if (account.Password == pin)
                 {
-                    Accno.Balance -= amount;
-                    var Accno1 = this.accountslist.Single(m => m.AccountNumber == raccnumber);
-                    Accno1.Balance += amount;
-                    AddTransaction(saccnumber, "withdraw", DateTime.Now.ToString(), amount);
-                    AddTransaction(raccnumber, "deposit", DateTime.Now.ToString(), amount);
-
-
+                    account.Balance += amount;
+                    AddTransaction(accountnumber, "deposit", DateTime.Now, amount);
                 }
                 else
-                    throw new Exception("Insufficient balance");
+                {
+                    throw new Exception("Incorrect pin number");
+                }
+            }
+
+            return true;
+        }
+        public bool WithDraw(double amount, int accountnumber, int pin)
+        {
+            var account = this.bank.AccountsList.SingleOrDefault(m => m.AccountNumber == accountnumber);
+
+            if (account is null)
+            {
+                throw new Exception("Either invalid");
             }
             else
             {
-                throw new Exception("Invalid pin number");
+                if (account.Password == pin)
+                {
+
+                    if (account.Balance < amount)
+                    {
+                        throw new Exception("Insufficient Balance");
+                    }
+                    else
+                    {
+
+                        account.Balance -= amount;
+                        AddTransaction(accountnumber, "withdraw", DateTime.Now, amount);
+                    }
+                }
+
+
+                else
+                {
+                    throw new Exception("Incorrect pin number");
+                }
             }
             return true;
-            
-           
+
         }
-        public int remBalance(int accountnumber, int pin)
+        public bool transferAmount(double amount, int senderaccnumber, int pin, int receiveraccnumber)
+        {
+            var account = this.bank.AccountsList.SingleOrDefault(m => m.AccountNumber == senderaccnumber);
+
+            if (account is null)
+            {
+                throw new Exception("Either invalid");
+            }
+            else
+            {
+                if (account.Password == pin)
+                {
+
+                    if (account.Balance > amount)
+                    {
+                        account.Balance -= amount;
+                        var receiveraccount = this.bank.AccountsList.SingleOrDefault(m => m.AccountNumber == receiveraccnumber);
+                        receiveraccount.Balance += amount;
+                        AddTransaction(senderaccnumber,"WithDraw", DateTime.Now, amount);
+                        AddTransaction(receiveraccnumber, "deposit", DateTime.Now, amount);
+
+
+                    }
+                    else
+                        throw new Exception("Insufficient balance");
+                    
+                }
+
+                else
+                {
+                    throw new Exception("Invalid pin number");
+                }
+            }
+            return true;
+
+
+        }
+        public double remBalance(int accountnumber, int pin)
         {
 
-            var Accno = this.accountslist.Single(m => m.AccountNumber == accountnumber);
-            if (Accno.pin == pin)
-                return Accno.Balance;
+            var account = this.bank.AccountsList.SingleOrDefault(m => m.AccountNumber == accountnumber);
+
+            if (account is null)
+            {
+                throw new Exception("Either invalid");
+            }
             else
-                throw new Exception("Invalid pin number");
+            {
+                if (account.Password == pin)
+                    return account.Balance;
+
+                else
+                    throw new Exception("Invalid pin number");
+            }
         }
-        public void AddTransaction(int toid,string type,string Datetime,int amount)
+        public void AddTransaction(int targetid, string type, DateTime datetime, double amount)
         {
-            
-            Transaction transaction = new(toid, type, amount, Datetime);
-            transaclist.Add(transaction);
+            var account = this.bank.AccountsList.SingleOrDefault(m => m.AccountNumber == targetid);
+            Transaction transaction = new(targetid, type, amount, datetime.ToString());
+            account.Transactions.Add(transaction);
         }
-        
+        public void GetTransactionhistory(int acnumber)
+        {
+            var account = bank.AccountsList.SingleOrDefault(m => m.AccountNumber == acnumber);
+            if (account is null)
+            {
+                Console.WriteLine("account number is invalid");
+            }
+            else
+            {
+                foreach (Transaction transaction in account.Transactions)
+                {
+                    Console.WriteLine(transaction.Datetime + " " + transaction.Type);
+                    if (transaction.Type == "deposit")
+                        Console.WriteLine("credited+" + transaction.Amount);
+                    else
+                        Console.WriteLine("debited- " + transaction.Amount);
+                    Console.WriteLine("");
+
+                }
+            }
+
         }
-        
-  }
+    }
+
+}
 
 
